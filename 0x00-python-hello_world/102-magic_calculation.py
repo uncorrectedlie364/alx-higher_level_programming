4f88c63f701e119fb2e14b97292b6dff1ce1ecf7
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-05 02:25:14 -0700

    Calculating using bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,14 +1,3 @@
-import dis
-
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    # Load the constant 98
-    result = 98
-    
-    # Calculate a ** b
-    result = result + (a ** b)
-    
-    # Return the result
-    return result
-
-# Disassemble the bytecode to verify
-dis.dis(magic_calculation)
+    return (98 + a ** b)
