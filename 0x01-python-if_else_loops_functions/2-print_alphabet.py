5ebf17f7d5907ade913945eadcf4b44e8e9bd3e7
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-10 02:12:39 -0700

    printing the ASCII alphabet in lowercase using a loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for ascii_value in range(97, 123):
-    character = chr(ascii_value)
-    print(character, end=" ")
+for ascii_value in range(ord('a'), ord('z') + 1):
+    print(chr(ascii_value), end="")
