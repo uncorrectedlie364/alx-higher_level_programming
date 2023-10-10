0dbcac24a5bc9a7142aaff2ba3d74aa2c3e24d2b
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-10 02:37:11 -0700

    Printing the ASCII alphabet in lowercase ecluding q & e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_value in range(ord('a'), ord('z') + 1):
     if chr(ascii_value) not in ('e', 'q'):
-        print(chr(ascii_value), end="")
+        print("{}".format(chr(ascii_value)), end="")
