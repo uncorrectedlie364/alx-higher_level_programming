c7dcf2a0a2ed4220696bb5099cf604e0cc8edc3e
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-11 16:36:06 -0700

    progrpringing the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
-from sys import argv
 
 if __name__ == "__main__":
-    num_arguments = len(argv) - 1  # Subtract 1 to exclude the script name
 
-    if num_arguments == 0:
-        print("Number of argument(s): 0.")
-        print(".")
+    """Print the number of and list of arguments."""
+
+    import sys
+
+
+    count = len(sys.argv) - 1
+
+    if count == 0:
+
+        print("0 arguments.")
+
+    elif count == 1:
+
+        print("1 argument:")
+
     else:
-        print("Number of argument(s):", num_arguments)
-        print("Arguments:", end=' ')
-        if num_arguments == 1:
-            print(":", end='\n')
-        else:
-            print("s:", end='\n')
 
-        for i, arg in enumerate(argv[1:], start=1):
-            print(f"{i}: {arg}")
+        print("{} arguments:".format(count))
+
+    for i in range(count):
+
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
