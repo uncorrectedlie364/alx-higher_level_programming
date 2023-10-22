24694499b5c303c0cae61e0eb51aa8a4338dfa14
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-22 05:54:09 -0700

    matrix update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,20 +6,19 @@ def print_matrix_integer(matrix=[[]]):
     Args:
         matrix: a matrix
     """
-
-# If no argument is supplied print a newline
+    # If no argument is supplied print a newline
     if not matrix:
         print()
     else:
-# Loop through the rows
+        # Loop through the rows
         for row in matrix:
-# Loop through the items in row
+            # Loop through the items in row
             for item in row:
-# Add space between items if not last item in row
+                # Add space between items if not last item in row
                 if row.index(item) != len(row) - 1:
                     endspace = " "
                 else:
                     endspace = ""
-# Print the items in the row
+                    # Print the items in the row
                 print("{:d}".format(item), end=endspace)
             print()
