d649a2250c6173726ac2c9d044cf829a40a905ef
uncorrectedlie364 <mcclarerodgy@gmail.com>
2023-10-22 05:58:53 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    """
-    Args:
-        sentence: a string argument
-
-    Returns:
-        a tuple with the length of a string and its first character
-    """
-
-    str_len, first_char = len(sentence), sentence[0]
-    return (str_len, first_char)
+    total = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new = total, char
+    return(new)
