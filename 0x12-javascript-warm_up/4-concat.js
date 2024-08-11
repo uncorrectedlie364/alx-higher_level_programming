13b7b3d377e0905b92a39985e19805bf442a5ed4
uncorrectedlie364 <mcclarerodgy@gmail.com>
2024-08-11 14:56:35 -0700

    concating a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+const arg1 = args[0] !== undefined ? args[0] : 'undefined';
+const arg2 = args[1] !== undefined ? args[1] : 'undefined';
+
+console.log('${arg1} is {arg2}');
