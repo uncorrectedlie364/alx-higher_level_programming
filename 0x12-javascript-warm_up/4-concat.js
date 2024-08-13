a6d0e9926354245659d6f2f636abf4956e03c1de
uncorrectedlie364 <eterno_vesnic@Eterno>
2024-08-13 19:21:16 +0200

    revised concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-const arg1 = args[0] !== undefined ? args[0] : 'undefined';
-const arg2 = args[1] !== undefined ? args[1] : 'undefined';
-
-console.log('${arg1} is {arg2}');
+console.log(process.argv[2] + ' is ' + process.argv[3]);
