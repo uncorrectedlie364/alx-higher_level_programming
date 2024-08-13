bf0db75ff783c93cda5cfa831c722b6ab333c2ea
uncorrectedlie364 <eterno_vesnic@Eterno>
2024-08-13 19:46:42 +0200

    passing an argument as an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-const argument = process.argv[2];
-const number = Number(argument);
-
-if (!isNAn(number)) {
-	console.log('My number: ${Math.floor(number)}');
-} else {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
 	console.log('Not a number');
+} else {
+	console.log('My number:', parseInt(process.argv[2]));
 }
